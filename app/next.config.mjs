187d1b7b4a9296ba4f2b94be48dc9a8f8ecafd47
ebinycoop <94120483+ebinycoop@users.noreply.github.com>
2026-06-06 15:13:59 -0400

    Create next.config.mjs

---
+++ app/next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Prevents compilation errors regarding node-specific secure encryption streams
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        os: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
